5b7b5d1b3722d89d835c3becdb9a2e51d076e44a
AthiraaAthi <athiraa2612@gmail.com>
2025-03-04 18:28:54 +0530

    updated image paths to use forward slashes and add girl1 image constant

--- lib/utils/image_constant/imageconstant.dart
+++ lib/utils/image_constant/imageconstant.dart
@@ -1,5 +1,6 @@
-const String profile = "assets\images\profile.png";
-const String boy1 = "assets\images\boy1.png";
-const String boy2 = "assets\images\boy2.png";
-const String boy3 = "assets\images\boy3.png";
-const String girls = "assets\images\girls.png";
+const String profile = "assets/images/profile.png";
+const String boy1 = "assets/images/boy1.png";
+const String boy2 = "assets/images/boy2.png";
+const String boy3 = "assets/images/boy3.png";
+const String girls = "assets/images/girls.png";
+const String girl1 = "assets/images/girl1.png";
