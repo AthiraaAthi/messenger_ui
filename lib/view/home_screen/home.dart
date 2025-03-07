fac7c6abf281f0947a24b0431b3b4745724405fb
AthiraaAthi <athiraa2612@gmail.com>
2025-03-07 18:04:19 +0530

    refactor HomeScreen layout to enhance scrolling and improve chat display

--- lib/view/home_screen/home.dart
+++ lib/view/home_screen/home.dart
@@ -80,125 +80,127 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            Container(
-              height: 40,
-              width: double.infinity,
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: grey,
-                borderRadius: BorderRadius.circular(10),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Container(
+                height: 40,
+                width: double.infinity,
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: grey,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.search,
+                      color: Colors.grey,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text("Search",
+                        style: TextStyle(color: Colors.grey, fontSize: 18)),
+                  ],
+                ),
               ),
-              child: Row(
-                children: [
-                  Icon(
-                    Icons.search,
-                    color: Colors.grey,
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text("Search",
-                      style: TextStyle(color: Colors.grey, fontSize: 18)),
-                ],
+              SizedBox(
+                height: 20,
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              height: 150,
-              child: ListView.builder(
+              Container(
+                height: 150,
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: stories.length,
+                  itemBuilder: (context, index) => Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        CircleAvatar(
+                          radius: 30,
+                          backgroundColor: grey,
+                          backgroundImage: stories[index]['isAdd'] == 'true'
+                              ? null
+                              : AssetImage(stories[index]['image']!),
+                          child: stories[index]['isAdd'] == 'true'
+                              ? Icon(Icons.add, size: 30, color: Colors.black)
+                              : null,
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          stories[index]['name']!,
+                          style: TextStyle(
+                              color: Colors.grey, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              ListView.builder(
+                scrollDirection: Axis.vertical,
+                itemCount: chats.length,
                 shrinkWrap: true,
-                scrollDirection: Axis.horizontal,
-                itemCount: stories.length,
                 itemBuilder: (context, index) => Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      CircleAvatar(
-                        radius: 30,
-                        backgroundColor: grey,
-                        backgroundImage: stories[index]['isAdd'] == 'true'
-                            ? null
-                            : AssetImage(stories[index]['image']!),
-                        child: stories[index]['isAdd'] == 'true'
-                            ? Icon(Icons.add, size: 30, color: Colors.black)
-                            : null,
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                        stories[index]['name']!,
-                        style: TextStyle(
-                            color: Colors.grey, fontWeight: FontWeight.bold),
-                      ),
-                    ],
+                  child: Container(
+                    color: Colors.amber,
+                    padding: EdgeInsets.all(
+                      10,
+                    ),
+                    height: 80,
+                    width: double.infinity,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        CircleAvatar(
+                          radius: 30,
+                          backgroundColor: grey,
+                          backgroundImage: AssetImage(stories[index]['image']!),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              chats[index]['name']!,
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                            Text(
+                              chats[index]['message']! +
+                                  " . " +
+                                  chats[index]['time']!,
+                              style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w400),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          width: 70,
+                        ),
+                        Icon(
+                          Icons.check_circle_outline,
+                          color: Colors.grey,
+                        )
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ),
-            ListView.builder(
-              scrollDirection: Axis.vertical,
-              itemCount: chats.length,
-              shrinkWrap: true,
-              itemBuilder: (context, index) => Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  color: Colors.amber,
-                  padding: EdgeInsets.all(
-                    10,
-                  ),
-                  height: 80,
-                  width: double.infinity,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      CircleAvatar(
-                        radius: 30,
-                        backgroundColor: grey,
-                        backgroundImage: AssetImage(stories[index]['image']!),
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            chats[index]['name']!,
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 17,
-                                fontWeight: FontWeight.w700),
-                          ),
-                          Text(
-                            chats[index]['message']! +
-                                " . " +
-                                chats[index]['time']!,
-                            style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 15,
-                                fontWeight: FontWeight.w400),
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        width: 80,
-                      ),
-                      Icon(
-                        Icons.check_circle_outline,
-                        color: Colors.grey,
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
