2f3c62d9140980ab684d57a3e891a4e3a58e0585
AthiraaAthi <athiraa2612@gmail.com>
2025-03-08 18:07:15 +0530

    added HomeScreen to the BottomNav state screens list

--- lib/view/bottom_nav/bottom_nav..dart
+++ lib/view/bottom_nav/bottom_nav..dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:messenger_ui/view/home_screen/home.dart';
 
 class BottomNav extends StatefulWidget {
   const BottomNav({super.key});
@@ -9,7 +10,9 @@ class BottomNav extends StatefulWidget {
 
 class _BottomNavState extends State<BottomNav> {
   int _selectedIndex = 0;
-  List<Widget> screens = [];
+  List<Widget> screens = [
+    HomeScreen(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold();
