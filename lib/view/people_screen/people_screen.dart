4c8328fdc80977f97f3af6801fd9351300384991
AthiraaAthi <athiraa2612@gmail.com>
2025-03-10 17:26:16 +0530

    added color and image constants imports; modified AppBar in PeopleScreen for improved layout

--- lib/view/people_screen/people_screen.dart
+++ lib/view/people_screen/people_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:messenger_ui/utils/color_constant/colorconstant.dart';
+import 'package:messenger_ui/utils/image_constant/imageconstant.dart';
 
 class PeopleScreen extends StatelessWidget {
   const PeopleScreen({super.key});
@@ -6,7 +8,11 @@ class PeopleScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.all(5),
+        ),
+      ),
     );
   }
 }
