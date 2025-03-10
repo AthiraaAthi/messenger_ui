2c6df1c200a843e09828bee864fba6266465942d
AthiraaAthi <athiraa2612@gmail.com>
2025-03-10 17:22:51 +0530

    added AppBar to GroupsScreen for improved navigation

--- lib/view/groups_screen/groups_screen.dart
+++ lib/view/groups_screen/groups_screen.dart
@@ -5,6 +5,8 @@ class GroupsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
