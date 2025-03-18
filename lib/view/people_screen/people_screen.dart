86176f2ce50c8ecdb1f89597ee8c919e268636bb
AthiraaAthi <athiraa2612@gmail.com>
2025-03-18 16:45:14 +0530

    added radius to CircleAvatar in PeopleScreen for improved visual consistency

--- lib/view/people_screen/people_screen.dart
+++ lib/view/people_screen/people_screen.dart
@@ -288,7 +288,9 @@ class _PeopleScreenState extends State<PeopleScreen> {
                   fontWeight: FontWeight.w600,
                 ),
               ),
-              trailing: CircleAvatar(),
+              trailing: CircleAvatar(
+                radius: 20,
+              ),
             )
           ],
         ),
