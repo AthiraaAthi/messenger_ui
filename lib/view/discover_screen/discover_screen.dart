a4daf57ee89a2c203177add84b68e9f049d79351
AthiraaAthi <athiraa2612@gmail.com>
2025-03-19 16:53:04 +0530

    added background color to Scaffold in DiscoverScreen for improved aesthetics

--- lib/view/discover_screen/discover_screen.dart
+++ lib/view/discover_screen/discover_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:messenger_ui/utils/color_constant/colorconstant.dart';
 
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: white,
+    );
   }
 }
