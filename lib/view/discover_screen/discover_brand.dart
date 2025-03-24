bbddf4d57b9149f71f6a1351faaa681b7c5324ee
AthiraaAthi <athiraa2612@gmail.com>
2025-03-24 13:16:29 +0530

    updated DiscoverBrandWidget to accept image and brandName parameters

--- lib/view/discover_screen/discover_brand.dart
+++ lib/view/discover_screen/discover_brand.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
 class DiscoverBrandWidget extends StatelessWidget {
-  const DiscoverBrandWidget({super.key});
-
+  const DiscoverBrandWidget(
+      {super.key, required this.image, required this.brandName});
+  final String image;
+  final String brandName;
   @override
   Widget build(BuildContext context) {
     return Scaffold();
