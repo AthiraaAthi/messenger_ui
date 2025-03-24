22ebe9a78abf67449430e9a597a2a06a46735ac0
AthiraaAthi <athiraa2612@gmail.com>
2025-03-24 13:15:23 +0530

    add DiscoverBrandWidget for brand discovery feature implementation

--- lib/view/discover_screen/discover_brand.dart
+++ lib/view/discover_screen/discover_brand.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
 
+class DiscoverBrandWidget extends StatelessWidget {
+  const DiscoverBrandWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
