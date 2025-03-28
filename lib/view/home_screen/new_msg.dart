0fa3b8d7f83617ceb4e4356d926d5a3430c420cc
AthiraaAthi <athiraa2612@gmail.com>
2025-03-28 17:34:39 +0530

    centered title in AppBar of NewMessageScreen widget

--- lib/view/home_screen/new_msg.dart
+++ lib/view/home_screen/new_msg.dart
@@ -7,6 +7,7 @@ class NewMessageScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text("New Message"),
       ),
     );
