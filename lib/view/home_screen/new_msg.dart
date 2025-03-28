bca623b6ce02f84444118ed2cc5730a0661a28b6
AthiraaAthi <athiraa2612@gmail.com>
2025-03-28 17:33:08 +0530

    added NewMessageScreen widget with placeholder implementation

--- lib/view/home_screen/new_msg.dart
+++ lib/view/home_screen/new_msg.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
 
+class NewMessageScreen extends StatelessWidget {
+  const NewMessageScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
