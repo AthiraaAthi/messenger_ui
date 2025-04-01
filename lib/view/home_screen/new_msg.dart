f7ad6d221c768cfb4e2f5568da0e6bb7912e4021
AthiraaAthi <athiraa2612@gmail.com>
2025-04-01 16:53:41 +0530

    added color constant import to NewMessageScreen for improved styling options

--- lib/view/home_screen/new_msg.dart
+++ lib/view/home_screen/new_msg.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:messenger_ui/utils/color_constant/colorconstant.dart';
 import 'package:messenger_ui/utils/image_constant/imageconstant.dart';
 
 class NewMessageScreen extends StatelessWidget {
