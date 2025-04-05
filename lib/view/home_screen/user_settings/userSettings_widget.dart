d324db8683a77c8b5ef0c1113decd35f65098b3e
AthiraaAthi <athiraa2612@gmail.com>
2025-04-05 17:12:28 +0530

    added dark mode toggle UI with CircleAvatar and updated layout structure

--- lib/view/home_screen/user_settings/userSettings_widget.dart
+++ lib/view/home_screen/user_settings/userSettings_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class UsersettingsWidget extends StatelessWidget {
   const UsersettingsWidget({super.key});
@@ -6,7 +7,23 @@ class UsersettingsWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [],
+      children: [
+        ListTile(
+          leading: CircleAvatar(
+            radius: 25,
+            backgroundImage: AssetImage(""),
+          ),
+          title: Text(
+            "Dark Mode",
+            style: TextStyle(fontWeight: FontWeight.w500),
+          ),
+          trailing: CupertinoSwitch(
+            value: false,
+            onChanged: (value) {},
+          ),
+        ),
+        Divider()
+      ],
     );
   }
 }
