c6d7df4c794cdc1120ca71bad4055d9fef4be227
AthiraaAthi <athiraa2612@gmail.com>
2025-04-10 16:57:31 +0530

    refactored leading widget in AppBar to use InkWell for improved interactivity

--- lib/view/people_screen/people_screen.dart
+++ lib/view/people_screen/people_screen.dart
@@ -73,12 +73,14 @@ class _PeopleScreenState extends State<PeopleScreen> {
       appBar: AppBar(
         leading: Padding(
           padding: const EdgeInsets.all(5),
-          child: CircleAvatar(
-            backgroundImage: AssetImage(
-              profile,
+          child: InkWell(
+            child: CircleAvatar(
+              backgroundImage: AssetImage(
+                profile,
+              ),
+              radius: 20,
+              backgroundColor: black,
             ),
-            radius: 20,
-            backgroundColor: black,
           ),
         ),
         backgroundColor: white,
